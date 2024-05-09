5f884fb1689fe1540bab47191cc31e47ae464f7b
ban <gtoyoung@naver.com>
2024-05-09 17:59:32 +0900

    메인 토토로 추가

---
+++ components/home/homeMain.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Box } from "@chakra-ui/react";
+import { Totoro } from "components/web3D/Totoro";
+
+export const HomeMain = () => {
+  return (
+    <>
+      <Box>
+        <Totoro />
+      </Box>
+    </>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { HomeMain } from "components/home/homeMain";
+
 export default function Home() {
-  return <></>;
+  return <HomeMain />;
 }
